ac4bb373bfeeec5123be92948c1d8554600351d2
António Lima <tocalagarto93@gmail.com>
2016-03-31 16:18:44 +0100

    actually, correlation yelds more visually readable resuts
    - even though they are the "same" thing

---
+++ code/correlation_covariance.m
@@ -0,0 +1,34 @@
+function [ correlation_covariance_out ] = correlation_covariance( X, labels )
+%CORRELATION Summary of this function goes here
+%   Detailed explanation goes here
+
+% get covariance matrix of data
+cov_X = cov(X');
+figure
+imagesc(cov_X)
+
+ax = gca;
+ax.XTickLabel = labels;
+ax.YTickLabel = labels;
+ax.XTickLabelRotation = 90;
+xlabel('Feature')
+ylabel('Feature')
+title('Feature Covariance') 
+
+% get correlation matrix of data
+cor_X = corrcoef(X');
+figure
+imagesc(cor_X)
+
+ax = gca;
+ax.XTickLabel = labels;
+ax.YTickLabel = labels;
+ax.XTickLabelRotation = 90;
+xlabel('Feature')
+ylabel('Feature')
+title('Feature Correlation')
+
+correlation_covariance_out = struct('covariance', cov_X, 'correlation', cor_X);
+
+end
+
