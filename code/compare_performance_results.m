decd80608174c8237108cae0a55aa5a8d945df12
António Lima <tocalagarto93@gmail.com>
2016-04-15 18:33:14 +0100

    removed junk images

--- code/compare_performance_results.m
+++ code/compare_performance_results.m
@@ -15,163 +15,34 @@ num_classifier_labels = length(classifier_labels);
 %cpa_label_groups = {'Condition Analysis', 'Accuracy' 'True Condition', 'Prevalence', 'Predicted Condition', 'Diagnostic Analysis'};
 %cpa_labels = {'True Positives', 'False Positives', 'False Negatives', 'True Negatives', 'Accuracy', 'True Positive Rate', 'False Negative Rate', 'False Positive Rate', 'True Negative Rate', 'Prevalence', 'Positive Likelyhood Ratio', 'Negative Likelyhood Ratio', 'Diagnostic Odds Ratio'};
 
-best_tta_cpa_accuracy = zeros(num_tta_labels, num_classifier_labels);
-best_tta_cpa_prevalence = zeros(num_tta_labels, num_classifier_labels);
-best_tta_cpa_sensitivity = zeros(num_tta_labels, num_classifier_labels);
-best_tta_cpa_specificity = zeros(num_tta_labels, num_classifier_labels);
+accuracy = zeros(num_tta_labels, num_classifier_labels);
+prevalence = zeros(num_tta_labels, num_classifier_labels);
+sensitivity = zeros(num_tta_labels, num_classifier_labels);
+specificity = zeros(num_tta_labels, num_classifier_labels);
 
 for i = 1:num_tta_labels
 
     first_stats = tta_out.( tta_labels{i} ).( classifier_labels{1} ).cpa_out;
     
-    accuracy = zeros( num_classifier_labels, 1);
-    prevalence = zeros( num_classifier_labels, 1);
-    sensitivity = zeros( num_classifier_labels, 1);
-    specificity = zeros( num_classifier_labels, 1);
-    
     for j = 1:num_classifier_labels
    
         stats = tta_out.( tta_labels{i} ).( classifier_labels{j} ).cpa_out;
         
-        accuracy(j) = stats.accuracy;
-        prevalence(j) = stats.prevalence;
-        sensitivity(j) = stats.true_condition(1);
-        specificity(j) = stats.true_condition(4);
+        accuracy(i, :) = stats.accuracy;
+        prevalence(i, :) = stats.prevalence;
+        sensitivity(i, :) = stats.true_condition(1);
+        specificity(i, :) = stats.true_condition(4);
         
     end
-    
-    % Chalk up the better classifier for each model
-    [~, best_j] = max(accuracy);
-    best_tta_cpa_accuracy(i, best_j) = 1;
-    
-    [~, best_j] = max(prevalence);
-    best_tta_cpa_prevalence(i, best_j) = 1;
-    
-    [~, best_j] = max(sensitivity);
-    best_tta_cpa_sensitivity(i, best_j) = 1;
-
-    [~, best_j] = max(specificity);
-    best_tta_cpa_specificity(i, best_j) = 1;
-    
-    %{
-    
-    % --------------------
-    % Colormap Comparisons
-    
-    % Condition Analysis
-    figure;
-    imagesc(condition_analysis)
-    colorbar
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = {'True Positives', 'False Positives', 'False Negatives', 'True Negatives'};
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Condition Analysis')
-    ax.YTickLabel = classifier_labels;
-    ax.YTick = 1:length(ax.YTickLabel);
-    ylabel('Classifier')
-    
-    % True Condition
-    figure;
-    imagesc(true_condition)
-    colorbar
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = {'True Positive Rate', 'False Negative Rate', 'False Positive Rate', 'True Negative Rate'};
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('True Condition')
-    ax.YTickLabel = classifier_labels;
-    ax.YTick = 1:length(ax.YTickLabel);
-    ylabel('Classifier')
-    
-    % Predicted Condition
-    figure;
-    imagesc(predicted_condition)
-    colorbar
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = {'Positive Prediction Value', 'False Discovery Rate', 'False Omission Rate', 'Negative Predictive Value'};
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Predicted Condition')
-    ax.YTickLabel = classifier_labels;
-    ax.YTick = 1:length(ax.YTickLabel);    
-    ylabel('Classifier')
-    
-    % Diagnostic Analysis
-    figure;
-    imagesc(diagnostic_analysis)
-    colorbar
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = {'Positive Likelyhood Ratio', 'Negative Likelyhood Ratio', 'Diagnostic Odds Ratio'};
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Diagnostic Analysis')
-    ax.YTickLabel = classifier_labels;
-    ax.YTick = 1:length(ax.YTickLabel);
-    ylabel('Classifier')
-    
-    % ------------------
-    % Linear Comparisons
-    
-    % Accuracy
-    figure;
-    plot(accuracy)
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = classifier_labels;
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Classifier')
-    ylabel('Accuracy')
-    
-    % Prevalence
-    figure;
-    plot(prevalence)
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = classifier_labels;
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Classifier')
-    ylabel('Prevalence')
-    
-    % Sensitivity
-    figure;
-    plot(sensitivity)
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = classifier_labels;
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Classifier')
-    ylabel('Sensitivity')
-    
-    % Specificity
-    figure;
-    plot(specificity)
-    
-    title(tta_labels{i})
-    ax = gca;
-    ax.XTickLabel = classifier_labels;
-    ax.XTick = 1:length(ax.XTickLabel);
-    xlabel('Classifier')
-    ylabel('Specificity')
-        
-    close all;
-    %}
-    
+ 
 end
 
-% Better Accuracy
+% Accuracy
 figure;
-imagesc(best_tta_cpa_accuracy)
+imagesc(accuracy)
 colorbar
 
-title('Better Accuracy')
+title('Accuracy')
 ax = gca;
 ax.YTickLabel = tta_labels;
 ax.XTickLabel = classifier_labels;
@@ -180,12 +51,12 @@ ax.YTick = 1:length(ax.YTickLabel);
 ylabel('Data Model')
 xlabel('Classifier')
 
-% Better Prevalence
+% Prevalence
 figure;
-imagesc(best_tta_cpa_prevalence)
+imagesc(prevalence)
 colorbar
 
-title('Better Prevalence')
+title('Prevalence')
 ax = gca;
 ax.YTickLabel = tta_labels;
 ax.XTickLabel = classifier_labels;
@@ -194,12 +65,12 @@ ax.YTick = 1:length(ax.YTickLabel);
 ylabel('Data Model')
 xlabel('Classifier')
 
-% Better Sensitivity
+% Sensitivity
 figure;
-imagesc(best_tta_cpa_sensitivity)
+imagesc(sensitivity)
 colorbar
 
-title('Better Sensitivity')
+title('Sensitivity')
 ax = gca;
 ax.YTickLabel = tta_labels;
 ax.XTickLabel = classifier_labels;
@@ -208,12 +79,12 @@ ax.YTick = 1:length(ax.YTickLabel);
 ylabel('Data Model')
 xlabel('Classifier')
 
-% Better Specificity
+% Specificity
 figure;
-imagesc(best_tta_cpa_specificity)
+imagesc(specificity)
 colorbar
 
-title('Better Specificity')
+title('Specificity')
 ax = gca;
 ax.YTickLabel = tta_labels;
 ax.XTickLabel = classifier_labels;
